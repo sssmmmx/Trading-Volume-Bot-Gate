08accef62477e357c96afba6c5efd848d4d68e8e
sssmmmx <s3m3x@foxmail.com>
2022-03-06 11:33:39 +0800

    Add files via upload

--- trading_volume_bot_1.py
+++ trading_volume_bot_1.py
@@ -1,15 +1,15 @@
 # !/usr/bin/env python
 # -*- coding = utf-8 -*-
-# @time:2022/3/6 10:31
+# @time:2022/3/6 10:32
 # Author:ldx
-# @File:trading_volume_bot_0.py
+# @File:trading_volume_bot_1.py
 # @Software:PyCharm
 
 """
-方案0：
-市价同进同出。
-消耗预估：手续费0.05% + 损耗0.06% 合计约 0.15%
-综合计算：返佣前 275 返佣后 225
+方案1：
+价差刷量，同进同出。
+代码逻辑：对于同一个永续合约标的 获取盘口的买一和卖一，计算出委托价格，循环开多单和空单。
+消耗预估：手续费0.05% + 损耗0.05% 合计约 0.1%
 时间周期：较快
 """
 
@@ -42,8 +42,11 @@ while t < 5:
     # 监测 卖1 和 买1 的价差大于0.1%
     if (float(book_info["asks"][0]["p"]) - float(book_info["bids"][0]["p"])) / float(book_info["bids"][0]["p"]) > 0.001:
         print("进行下单")
-        buy = futures_api.create_orders(symbol, 1, "0", "ioc")
-        sell = futures_api.create_orders(symbol, -1, "0", "ioc")
+        px = (float(book_info["asks"][0]["p"]) + float(book_info["bids"][0]["p"])) / 2
+        f = f".{order_price_round.count('0')}f"
+        px = f"{format(px, f)}"
+        buy = futures_api.create_orders(symbol, 1, px, "gtc")
+        sell = futures_api.create_orders(symbol, -1, px, "gtc")
 
         while True:
             # 检查是否完成交易 "status": "finished"
@@ -54,13 +57,23 @@ while t < 5:
             else:
                 time.sleep(1)
 
-# 进行平仓 市价
-a = futures_api.close_orders(symbol, "0", "close_long", "ioc")
-b = futures_api.close_orders(symbol, "0", "close_short", "ioc")
-
 while True:
     time.sleep(1)
-    # 检查是否完成交易 "status": "finished"
-    if futures_api.query_orders(a['id'])["status"] == "finished" and futures_api.query_orders(b['id'])["status"] == "finished":
-        print("已完成平仓")
-        break
+    book_info = futures_api.order_book(symbol)
+    # 监测 卖1 和 买1 的价差大于0.1%
+    if (float(book_info["asks"][0]["p"]) - float(book_info["bids"][0]["p"])) / float(book_info["bids"][0]["p"]) > 0.001:
+        print("进行下单")
+        px = (float(book_info["asks"][0]["p"]) + float(book_info["bids"][0]["p"])) / 2
+        f = f".{order_price_round.count('0')}f"
+        px = f"{format(px, f)}"
+
+        # 进行平仓 限价px
+        a = futures_api.close_orders(symbol, px, "close_long", "gtc")
+        b = futures_api.close_orders(symbol, px, "close_short", "gtc")
+
+        while True:
+            time.sleep(1)
+            # 检查是否完成交易 "status": "finished"
+            if futures_api.query_orders(a['id'])["status"] == "finished" and futures_api.query_orders(b['id'])["status"] == "finished":
+                print("已完成平仓")
+                break
